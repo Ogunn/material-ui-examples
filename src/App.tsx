b59f1158aef776f0ff559b1b1d351528b49eb0b4
Tiago Santos <tiago@am3.localdomain>
2019-10-30 19:13:12 -0300

    Add Routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import PaperGroup from "./components/PaperGroup";
+import BasicGrid from "./components/BasicGrid";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <PaperGroup />
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/paper-group">PaperGroup</Link>
+          </li>
+          <li>
+            <Link to="/basic-grid">BasicGrid</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/paper-group">
+          <PaperGroup />
+        </Route>
+        <Route path="/basic-grid">
+          <BasicGrid />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
